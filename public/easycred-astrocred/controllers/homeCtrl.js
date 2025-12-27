c94686d8aa715cc51878b0986d8778c908f99ce3
Siddharth Chandra <Siddharth.Chandra@motion.com>
2025-12-27 11:20:06 +0530

    Basic Onboarding

--- public/easycred-astrocred/controllers/homeCtrl.js
+++ public/easycred-astrocred/controllers/homeCtrl.js
@@ -1,13 +1,40 @@
-app.controller('homeCtrl', ['$scope', '$rootScope', '$timeout', 'stateManager', function($scope, $rootScope, $timeout, stateManager) {
+app.controller('homeCtrl', ['$scope', '$rootScope', '$timeout', 'stateManager', '$location', function($scope, $rootScope, $timeout, stateManager, $location) {
 
 
 
     $timeout(function() {
+
+
+        if (stateManager.isUserLogggedIn()) {
+            var userProfile = stateManager.getProfile();
+            log('User Profile :');
+            log(userProfile);
+
+            if (stateManager.isProfileCompleted()) {
+                log('Profile Completed :');
+
+
+                if (userProfile.consent.isTermsAccepted) {
+                    if (stateManager.isKYCCompleted()) {
+
+                    } else {}
+
+                } else {}
+            } else {
+                log('Profile Not Completed :');
+                $location.path("profile/complete");
+            }
+        } else {
+            $location.path("login");
+
+        }
+
         $rootScope.$on('request_error', function(event, data) {
             error('request_error');
             $scope.loader.hide();
-
         });
+
+
     });
 
 
